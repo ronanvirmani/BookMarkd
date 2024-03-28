6264aca91812b135da45c83ffb7a3cb7c2773819
Alex Johnson <alex@jeffjohnson.net>
2024-03-28 16:03:32 -0400

    Added page routing with react router

---
+++ frontend/src/pages/ProfilePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProfilePage() {
+    return (
+        <div>
+            <h2>BookMarkd Profile Page</h2>
+        </div>
+    );
+}
+
+export default ProfilePage;
