62beba9e842555a8b9ee404d099756edda5bfeea
BailBruni <baileybruni@ufl.edu>
2024-04-11 03:57:12 -0400

    Added Search Page as well as a search bar.  No functionality though

---
+++ frontend/src/pages/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchPage() {
+    return (
+        <div>
+            <h2>BookMarkd Search Page</h2>
+            <input type="text" className="text-center" placeholder="Search..."/>
+        </div>
+    );
+}
+
+export default SearchPage;
